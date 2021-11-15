445ac6f5023b5cbf5914ce74b612a6ebc65198dc
Matheus Oliveira <matheus_so@yahoo.com.br>
2021-11-15 14:13:34 -0300

    Style the function 2

--- test_package/lib.py
+++ test_package/lib.py
@@ -2,4 +2,7 @@ from termcolor import colored
 
 
 def try_me():
-    return colored('First package test by Matheus Sposito', 'magenta', attrs=['blink'])
+    text = colored('First package test by Matheus Sposito',
+                   'magenta',
+                   attrs=['blink'])
+    return print(text)
